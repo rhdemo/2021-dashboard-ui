c84340bb143785d1ea0ec79f80812abbccc8064a
Luke Dary <lukedary@gmail.com>
2021-04-23 21:07:00 -0400

    Replay work

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -1,7 +1,11 @@
 import { Drash } from "https://deno.land/x/drash@v1.4.4/mod.ts";
 
-const leaderboardUrl = Deno.env.get("LEADERBOARD_SERVER");
-const statsUrl = Deno.env.get("STATS_SERVER");
+const leaderboardUrl = 'leaderboard-battleships-leaderboard.apps.summit-gcp.eior.p2.openshiftapps.com'; // Deno.env.get("LEADERBOARD_SERVER");
+const statsUrl = 'leaderboard-battleships-leaderboard.apps.summit-gcp.eior.p2.openshiftapps.com'; // Deno.env.get("STATS_SERVER");
+
+// const leaderboardUrl = Deno.env.get("LEADERBOARD_SERVER");
+// const statsUrl = Deno.env.get("STATS_SERVER");
+
 
 export class DashboardResource extends Drash.Http.Resource {
     static paths = ["/","/dashboard"];
