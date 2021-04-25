04a6b78e2caff78af5fbb614f0b9bf06dc58c96a
Luke Dary <lukedary@gmail.com>
2021-04-25 14:31:05 -0400

    Replay boards

--- src/replay.ts
+++ src/replay.ts
@@ -40,7 +40,7 @@ type Match = {
 }
 
 const replayUrl = 'streams-replay-tracker-kafka-streams.apps.summit-aws-ue1.zch4.p1.openshiftapps.com';
-const rankingUrl = 'scoring-service-battleships-scoring.apps.summit-gcp.eior.p2.openshiftapps.com';
+//const rankingUrl = 'scoring-service-battleships-scoring.apps.summit-gcp.eior.p2.openshiftapps.com';
 
 // const replayUrl = Deno.env.get("REPLAY_SERVER");
 // const rankingUrl = Deno.env.get("RANK_SERVER");
@@ -54,6 +54,7 @@ export class ReplayResource extends Drash.Http.Resource {
         if (!gameId) { throw new Drash.Exceptions.HttpException(400, "replay requires a gameID")}
         else {
             // rankData = await fetch(`http://${rankingUrl}/scoring/${gameId}/ranking?max=10`).then(res=>res.json()).catch(e=>[]);
+            console.log(`https://${replayUrl}/game/${gameId}/replays`);
             replayData = await fetch(`https://${replayUrl}/game/${gameId}/replays`).then(res=>res.json()).then((matches:Match[]) => {
                 return matches.map((match:Match) : Turn[] => {
                     console.log('Turns:',match.turns.length);
@@ -70,9 +71,4 @@ export class ReplayResource extends Drash.Http.Resource {
             
         }
     }
-}
-
-const getReplayData = async (rankData: Player[], replayData: Promise<Turn[]>[]) => {
-    
-    return replayData;
 }
